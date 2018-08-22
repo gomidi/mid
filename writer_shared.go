497b4ffb4b42a374dfdca386ee0a4788ab316a1e
Marc Rene Arns <arns@stridor.net>
2018-08-22 12:34:16 +0200

    add midi note consolidation (on be default) for writers and a Silence method to kill hanging notes

--- writer_shared.go
+++ writer_shared.go
@@ -1,14 +1,18 @@
 package mid
 
 import (
+	"fmt"
+	"github.com/gomidi/cc"
 	"github.com/gomidi/midi"
 	"github.com/gomidi/midi/midimessage/channel"
 	"github.com/gomidi/midi/midimessage/sysex"
 )
 
 type midiWriter struct {
-	wr midi.Writer
-	ch channel.Channel
+	wr              midi.Writer
+	ch              channel.Channel
+	noteState       [16][127]bool
+	noConsolidation bool
 }
 
 // SetChannel sets the channel for the following midi messages
@@ -29,13 +33,21 @@ func (w *midiWriter) KeyPressure(key, pressure uint8) error {
 }
 
 // NoteOff writes a note off message for the current channel
+// By default, midi notes are consolidated (see ConsolidateNotes method)
 func (w *midiWriter) NoteOff(key uint8) error {
-	return w.wr.Write(w.ch.NoteOff(key))
+	return w.Write(w.ch.NoteOff(key))
+}
+
+// NoteOffVelocity writes a note off message for the current channel with a velocity.
+// By default, midi notes are consolidated (see ConsolidateNotes method)
+func (w *midiWriter) NoteOffVelocity(key, velocity uint8) error {
+	return w.Write(w.ch.NoteOffVelocity(key, velocity))
 }
 
 // NoteOn writes a note on message for the current channel
+// By default, midi notes are consolidated (see ConsolidateNotes method)
 func (w *midiWriter) NoteOn(key, veloctiy uint8) error {
-	return w.wr.Write(w.ch.NoteOn(key, veloctiy))
+	return w.Write(w.ch.NoteOn(key, veloctiy))
 }
 
 // PitchBend writes a pitch bend message for the current channel
@@ -95,6 +107,95 @@ func (w *midiWriter) SystemExclusive(data []byte) error {
 	return w.wr.Write(sysex.SysEx(data))
 }
 
+// silentium for a single channel
+func (w *midiWriter) silentium(ch uint8, force bool) (err error) {
+	for key, val := range w.noteState[ch] {
+		if force || val {
+			err = w.wr.Write(channel.Channel(ch).NoteOff(uint8(key)))
+		}
+	}
+
+	if force {
+		err = w.wr.Write(channel.Channel(ch).ControlChange(cc.AllNotesOff, 0))
+		err = w.wr.Write(channel.Channel(ch).ControlChange(cc.AllSoundOff, 0))
+	}
+	return
+}
+
+// Silence sends a  note off message for every running note
+// on the given channel. If the channel is -1, every channel is affected.
+// If note consolidation is switched off, notes aren't tracked and therefor
+// every possible note will get a note off. This could also be enforced by setting
+// force to true. If force is true, additionally the cc messages AllNotesOff (123) and AllSoundOff (120)
+// are send.
+// If channel is > 15, the method panics.
+// The error or not error of the last write is returned.
+// (i.e. it does not return on the first error, but tries everything instead to make it silent)
+func (w *midiWriter) Silence(ch int8, force bool) (err error) {
+	if ch > 15 {
+		panic("invalid channel number")
+	}
+	if w.noConsolidation {
+		force = true
+	}
+
+	// single channel
+	if ch >= 0 {
+		err = w.silentium(uint8(ch), force)
+		// set note states for the channel
+		w.noteState[ch] = [127]bool{}
+		return
+	}
+
+	// all channels
+	for c := 0; c < 16; c++ {
+		err = w.silentium(uint8(c), force)
+	}
+	// reset all note states
+	w.noteState = [16][127]bool{}
+	return
+}
+
+// ConsolidateNotes enables/disables the midi note consolidation (default: enabled)
+// When enabled, midi note on/off messages are consolidated, that means the on/off state of
+// every possible note on every channel is tracked and note on messages are only
+// written, if the corresponding note is off and vice versa. Note on messages
+// with a velocity of 0 behave the same way as note offs.
+// The tracking of the notes is not cross SMF tracks, i.e. a meta.EndOfTrack
+// message will reset the tracking.
+// The consolidation should prevent "hanging" notes in most cases.
+// If on is true, the note will be started tracking again (fresh state), assuming no note is currently running.
+func (w *midiWriter) ConsolidateNotes(on bool) {
+	if on {
+		w.noteState = [16][127]bool{}
+	}
+	w.noConsolidation = !on
+}
+
+// Write writes the given midi.Message. By default, midi notes are consolidated (see ConsolidateNotes method)
 func (w *midiWriter) Write(msg midi.Message) error {
+	if w.noConsolidation {
+		return w.wr.Write(msg)
+	}
+	switch m := msg.(type) {
+	case channel.NoteOn:
+		if m.Velocity() > 0 && w.noteState[m.Channel()][m.Key()] {
+			return fmt.Errorf("can't write %s. note already running.", msg)
+		}
+		if m.Velocity() == 0 && !w.noteState[m.Channel()][m.Key()] {
+			return fmt.Errorf("can't write %s. note is not running.", msg)
+		}
+		w.noteState[m.Channel()][m.Key()] = m.Velocity() > 0
+	case channel.NoteOff:
+		if !w.noteState[m.Channel()][m.Key()] {
+			return fmt.Errorf("can't write %s. note is not running.", msg)
+		}
+		w.noteState[m.Channel()][m.Key()] = false
+	case channel.NoteOffVelocity:
+		if !w.noteState[m.Channel()][m.Key()] {
+			return fmt.Errorf("can't write %s. note is not running.", msg)
+		}
+		w.noteState[m.Channel()][m.Key()] = false
+	}
 	return w.wr.Write(msg)
 }
